6532ea5282c967a3536586ec4693b3a05f7a4862
Sam Lade <sam@sentynel.com>
2016-02-10 22:56:01 +0000

    minimal hunspell binding

---
+++ build.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+from cffi import FFI
+ffi = FFI()
+
+ffi.set_source("_hunspell",
+        "#include <hunspell/hunspell.h>",
+        libraries=["hunspell"],)
+
+ffi.cdef("""
+typedef struct Hunhandle Hunhandle;
+
+Hunhandle *Hunspell_create(const char * affpath, const char * dpath);
+Hunhandle *Hunspell_create_key(const char * affpath, const char * dpath,
+    const char * key);
+void Hunspell_destroy(Hunhandle *pHunspell);
+int Hunspell_spell(Hunhandle *pHunspell, const char *);
+char *Hunspell_get_dic_encoding(Hunhandle *pHunspell);
+int Hunspell_suggest(Hunhandle *pHunspell, char*** slst, const char * word);
+int Hunspell_analyze(Hunhandle *pHunspell, char*** slst, const char * word);
+int Hunspell_stem(Hunhandle *pHunspell, char*** slst, const char * word);
+int Hunspell_stem2(Hunhandle *pHunspell, char*** slst, char** desc, int n);
+int Hunspell_generate(Hunhandle *pHunspell, char*** slst, const char * word,
+    const char * word2);
+int Hunspell_generate2(Hunhandle *pHunspell, char*** slst, const char * word,
+    char** desc, int n);
+int Hunspell_add(Hunhandle *pHunspell, const char * word);
+int Hunspell_add_with_affix(Hunhandle *pHunspell, const char * word, const char * example);
+int Hunspell_remove(Hunhandle *pHunspell, const char * word);
+void Hunspell_free_list(Hunhandle *pHunspell, char *** slst, int n);
+""")
+if __name__ == "__main__":
+    ffi.compile()
---
+++ hunspell.py
@@ -0,0 +1,44 @@
+import os
+
+from _hunspell import ffi, lib
+
+class HunspellError(Exception):
+    pass
+
+class Hunspell:
+    def __init__(self, path, language="en_US"):
+        """Initialise library. Takes a path to the .aff and .dic files, and the
+        language wanted."""
+        base = os.path.join(path, language)
+        aff = (base + ".aff").encode("utf8")
+        dic = (base + ".dic").encode("utf8")
+        if not os.path.exists(aff) and os.path.exists(dic):
+            raise FileNotFoundError("Couldn't find .aff and .dic files")
+        self.hun = lib.Hunspell_create(aff, dic)
+        if not self.hun:
+            raise HunspellError("Failed to initialise Hunspell")
+
+    def check(self, word):
+        """Returns True if the word is spelt correctly."""
+        if isinstance(word, str):
+            word = word.encode("utf8")
+        return bool(lib.Hunspell_spell(self.hun, word))
+
+    def suggest(self, word):
+        """Returns a list of suggestions for a specified word."""
+        enc = False
+        if isinstance(word, str):
+            word = word.encode("utf8")
+            enc = True
+        sl = ffi.new("char***")
+        n = lib.Hunspell_suggest(self.hun, sl, word)
+        if not n:
+            return []
+        res = [ffi.string(sl[0][i]) for i in range(n)]
+        if enc:
+            res = [i.decode("utf8") for i in res]
+        lib.Hunspell_free_list(self.hun, sl, n)
+        return res
+    
+    def __del__(self):
+        lib.Hunspell_destroy(self.hun)
